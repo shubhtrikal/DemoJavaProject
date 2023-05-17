af17670a5513e07253b8c93be9c410357b31fa5e
Shubh Asati <shubh.asati@BLRETV-FM33KMD6M.local>
2023-05-17 17:28:37 +0530

    initial commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author shubh.asati
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my first program");
+	}
+
+}
